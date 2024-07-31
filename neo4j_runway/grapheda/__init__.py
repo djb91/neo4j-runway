889016e0f215d4daac4153a65dbdd21db9725d0a
Daniel Bukowski <bukowski.daniel@gmail.com>
2024-07-31 09:41:53 -0500

    Initial grapheda work

---
+++ neo4j_runway/grapheda/__init__.py
@@ -0,0 +1,1 @@
+from .graphEDA import GraphEDA
--- neo4j_runway/grapheda/grapheda.py
+++ neo4j_runway/grapheda/grapheda.py
@@ -7,7 +7,7 @@ of the data in graph form (nodes, relationships, and properties).
 It also helps identify errors and outliers in the data. 
 """
 
-import pandas as pd
+# import pandas as pd
 
 import os
 from typing import Dict, List, Any, Union
@@ -25,7 +25,8 @@ class GraphEDA:
         password: Union[str, None] = None,
         uri: Union[str, None] = None,
         database: Union[str, None] = None,
-    )
+    
+    ):
 
         """
         The GraphEDA module performs Graph Exploratory data analysis on the created Neo4j graph database.
@@ -47,24 +48,32 @@ class GraphEDA:
         self.uri = uri
         self.database = database
 
-        # connect to Neo4j database using the credentials
-        # ref: https://neo4j.com/docs/api/python-driver/current/
-        def connect_to_neo4j(self):
-            """
-            Connect to the Neo4j database using the provided credentials.
-            """
-            if self.uri and self.username and self.password:
-                self.driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
-                print("Established connection to Neo4j")
-            else:
-                print("Connection details are missing")
+    # connect to Neo4j database using the credentials
+    # ref: https://neo4j.com/docs/api/python-driver/current/
+    def connect_to_neo4j(self):
+        """
+        Connect to the Neo4j database using the provided credentials.
+        """
+        if self.uri and self.username and self.password:
+            self.driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
+            print("Established connection to Neo4j")
+        else:
+            print("Connection details are missing")
 
-        # provide option to close the Neo4j connection
-        def close(self):
-            """
-            Close the connection to the Neo4j database.
-            """
-            if self.driver:
-                self.driver.close()
-                print("Connection to Neo4j database closed")
+    def confirm_neo4j_connection(self):
+        """
+        Confirm the connection to the Neo4j database.
+        """
+        self.driver.execute_query(
+            "RETURN datetime() AS currentTime"
+        )
+
+    # provide option to close the Neo4j connection
+    def close(self):
+        """
+        Close the connection to the Neo4j database.
+        """
+        if self.driver:
+            self.driver.close()
+            print("Connection to Neo4j database closed")
 
