b1a9dc4a450ee6ed5c1bbda2541fbdf44e0f8d61
Daniel Bukowski <bukowski.daniel@gmail.com>
2024-08-01 07:28:49 -0500

    GraphEDA minor changes and updates

--- neo4j_runway/grapheda/grapheda.py
+++ neo4j_runway/grapheda/grapheda.py
@@ -19,5 +19,21 @@ class GraphEDA:
     The GraphEDA module performs Graph Exploratory data analysis on the created Neo4j graph database.
     """
 
-
+    # will need to pass in data from constructing the graph 
+    def __init__(self, neo4j_graph: Neo4jGraph):
+        self.neo4j_graph = neo4j_graph
+        self.result_cache = dict() 
+    # explicit errors -- something didn't come over correctly
+        
+        # priority -- things in a graph created from the csv in this session
+            # which things are wrong versus the designed graph
+            # informative message to the user with pointers to exact issues 
+            # optional -- pass the data to an LLM to interpret 
+            # future -- have a method that will try to fix the issue  
+        
+        # future -- things in graph not created with runway 
+        
+        
+    # graph summary statistics
+    # other possible issues
 
