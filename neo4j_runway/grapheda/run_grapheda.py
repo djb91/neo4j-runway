40455449ff360e11857c6fcf8ff6782d38b36f0e
Daniel Bukowski <bukowski.daniel@gmail.com>
2024-08-06 18:27:07 -0500

    added queries to grapheda

--- neo4j_runway/grapheda/run_grapheda.py
+++ neo4j_runway/grapheda/run_grapheda.py
@@ -5,6 +5,8 @@ from neo4j_runway.database import Neo4jGraph
 from neo4j_runway.utils.read_env import read_environment
 from neo4j_runway.utils.test_connection import test_database_connection
 
+
+
 # import and read from .env file
 import dotenv 
 dotenv.load_dotenv()
@@ -21,16 +23,25 @@ dotenv.load_dotenv()
 
 from neo4j_runway.grapheda.grapheda import GraphEDA
 
-# neo4j_graph = Neo4jGraph()
 
+# instantiate Neo4jGraph
 neo4j_graph = Neo4jGraph(
         username=read_environment("NEO4J_USERNAME"),
         password=read_environment("NEO4J_PASSWORD"),
         uri=read_environment("NEO4J_URI"),
     )
 
-graph_eda = GraphEDA()
-response = graph_eda.count_node_labels()
-print(response)
 
+# instantiate GraphEDA class
+graph_eda = GraphEDA(neo4j_graph=neo4j_graph)
+
+#get node count by label 
+# result = graph_eda.count_node_labels()
+# print(result)
+
+# result = graph_eda.count_node_labels(as_df=False)
+# print(result)
+
+result = graph_eda.count_disconnected_nodes(as_df=False)
+print("disconnected node count:", result[0]['nodeCount'])
 
